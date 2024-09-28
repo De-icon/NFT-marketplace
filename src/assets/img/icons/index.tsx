163d50ff0aedbb5f6453bb49af20f1a0554b5ac0
Deicons <109363442+De-icon@users.noreply.github.com>
2024-09-28 17:59:07 +0100

    Update index.tsx

--- src/assets/img/icons/index.tsx
+++ src/assets/img/icons/index.tsx
@@ -1,4 +1,3 @@
 export { default as User } from './User.png'
-export { default as StoreFront } from './StoreFront.png'
 export { default as RocketLaunch } from './RocketLaunch.png'
 export { default as Logo } from './logo.png'
